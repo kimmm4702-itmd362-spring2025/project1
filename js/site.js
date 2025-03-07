399515414af25b8b60294800287bba072891a034
Kimberly Dominguez <kdominguez2@hawk.iit.edu>
2025-03-06 18:43:55 -0600

    updated JS

--- js/site.js
+++ js/site.js
@@ -3,6 +3,6 @@ jQuery(function($){
 	$("html").addClass("hasjs");
 	//if submit button is clicked, add new message
 	$('#input-submit').on('click',function(){
-		alter("Thank you for subscribing!");
+		$("submit").text("Thank you for subscribing!");
 		});
 });
