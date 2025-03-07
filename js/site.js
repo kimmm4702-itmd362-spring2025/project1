24ccf1f5eff2e7e90c39fe250ad868ad2f3298f6
Kimberly Dominguez <kdominguez2@hawk.iit.edu>
2025-03-06 19:29:29 -0600

    updated full JS

--- js/site.js
+++ js/site.js
@@ -2,21 +2,12 @@ jQuery(function($){
 	$('html').removeClass('nojs');
 	$("html").addClass("hasjs");
 
-	$("form").on("submit", function(event){
-		event.preventDefault(); // prevents form to refresh when submitted
-
-		let fname=$("#fname").val().trim()
-		let lname=$("#lname").val().trim()
-		let email=$("#email").val().trim()
-		let book=$("#book").val().
-
-		//if submit button is clicked, add new message
-		if(fname && lname && email && book){
-			alert('Thank you, $(fname) $(lname)! You subscribed to read book "$(book)"');
-			this.reset();
-		}else{
-			alert("Please fill out all the fields.");
-
+	$('#email').on('focus', function(){
+		$('#input-email label').addClass('active');
+	});
+	$('#email').on('blur', function(){
+		if($('#email').val().length == 0){
+			$('#input-email label').removeClass('active');
 		}
 	});
 });
