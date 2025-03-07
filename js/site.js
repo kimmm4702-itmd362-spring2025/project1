5ed098995860f665e4328938affba9fbc0f40112
Kimberly Dominguez <kdominguez2@hawk.iit.edu>
2025-03-06 20:09:47 -0600

    fixed JS with if statement

--- js/site.js
+++ js/site.js
@@ -2,15 +2,19 @@ jQuery(function($){
 	$('html').removeClass('nojs');
 	$("html").addClass("hasjs");
 
-	$('#signup-form').on('submit', function(event){
+	$("#signup-form").on("submit", function(event){
 		event.preventDefault(); 
 
-		const fname = $('#fname').val();
-		const lname = $('#lname').val();
-		const email = $('#email').val();
-		const book = $('#book').val();
+		const fname = $("#fname").val();
+		const lname = $("#lname").val();
+		const email = $("#email").val();
+		const book = $("#book").val();
 
-		alert(`Thank you, ${fname} ${lname}! You subscribed to read book "${book}"`);
+		if(fname && lname && email && book){
+			alert(`Thank you, ${fname} ${lname}! You subscribed to read book "${book}"`);
+		}else{
+			alert("Please fill in all the fields.");
+		}
 
 
 	});
