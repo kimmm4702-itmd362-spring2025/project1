4dc7f1cfee771b385f949fa0d50c6ad70f97c6e1
Kimberly Dominguez <kdominguez2@hawk.iit.edu>
2025-03-06 19:09:30 -0600

    updated for when input values are entered

--- js/site.js
+++ js/site.js
@@ -1,10 +1,19 @@
 jQuery(function($){
 	$('html').removeClass('nojs');
 	$("html").addClass("hasjs");
-	$("form").on("submit", function(){
+
+	$("form").on("submit", function(event){
+		event.preventDefault();
+
+		let fname=$("#fname").val().trim()
+		let lname=$("#lname").val().trim()
+		let email=$("#email").val().trim()
+		let book=$("#book").val().
+
 		//if submit button is clicked, add new message
 		if(fname && lname && email && book){
 			alert('Thank you, $(fname) $(lname)! You subscribed to read book "$(book)"');
+			this.reset();
 		}else{
 			alert("Please fill out all the fields.");
 
