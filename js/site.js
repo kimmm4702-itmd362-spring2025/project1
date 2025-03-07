0d85fcb890be2eea9fae023924af159ca2a9a609
Kimberly Dominguez <kdominguez2@hawk.iit.edu>
2025-03-06 18:59:57 -0600

    fixed JS added if statement

--- js/site.js
+++ js/site.js
@@ -1,8 +1,13 @@
 jQuery(function($){
 	$('html').removeClass('nojs');
 	$("html").addClass("hasjs");
-	//if submit button is clicked, add new message
-	$('#input-submit').on('click',function(){
-		$("submit").text("Thank you for subscribing!");
-		});
+	$("form").on("submit", function(){
+		//if submit button is clicked, add new message
+		if(fname && lname && email && book){
+			alter('Thank you, $(fname) $(lname)! You subscribed to read book "$(book)"');
+		}else{
+			alter("Please fill out all the fields.");
+
+		}
+	});
 });
